735b3d7fe0081fe573bc6d7a3b93d00b5621edf4
Austin W <awalte4@clemson.edu>
2021-09-28 14:26:29 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory_Systems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
