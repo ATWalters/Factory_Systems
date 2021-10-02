34fee86382decc90dbc7afb575a8f94103212099
Austin W <awalte4@clemson.edu>
2021-10-01 22:06:21 -0400

    Removed WriteLine statements from testing

--- Test.cs
+++ Test.cs
@@ -8,84 +8,107 @@ using System.Collections.Generic;
 namespace Factory_Systems{
     class Test{
 
-        //The number of measurements for a test to be considered valid        private static int CONSIDERED_VALID = 1000;
+        //The number of measurements for a test to be considered valid 
+        private static int CONSIDERED_VALID = 1000;
+        //Value for M
+        private static int M = 10;
+        //value for N
+        private static int N = 10;
+        //The ID of the test
         private int ID;
+        //The Plane ID of the test
         private string planeID;
+        //A list that holds all the measurements for the test
         private List<Measurement> data = new List<Measurement>();
-
+        //The height range of the test
         private double heightRange;
-
+        //The minimum height of the test
         private Measurement minHeight;
-
+        //The maximum height of the test
         private Measurement maxHeight;
-
+        //The mean height of the test
         private double meanHeight;
-
+        //The average roughness of the test
         private double avgRoughness;
-
+        //The root mean square roughness of the test
         private double rootMeanSqRoughness;
-
+        //If the test is a valid test or not
         private bool isValidTest;
-
+        //The number of measurements in the test
         private int numMeasurements = 0;
-
+        //The value of Mu for the test
         private double Mu;
 
+        //Two parameter constructor that takes in the test_uid as well as the PlaneID
         public Test(int ID, string planeID){
             this.ID = ID;
             this.planeID = planeID;
         }
 
+        //Returns if the test is a valid test or not
         public bool isValid(){
             return this.isValidTest;
         }
 
+        //Getter for the ID of the test
         public int getID(){
             return this.ID;
         }
 
+        //Getter for the PlaneID of the test
         public string getPlane(){
             return this.planeID;
         }
 
+        //Getter for the measurements in the test
         public List<Measurement> getData(){
             return this.data;
         }
 
+        //Getter for the maximum height
         public Measurement getMaxHeight(){
             return this.maxHeight;
         }
 
+        //Getter for the minimum height
         public Measurement getMinHeight(){
             return this.minHeight;
         }
 
+        //Getter for the height range
         public double getHeightRange(){
             return this.heightRange;
         }
 
+        //Getter for the mean height
         public double getMeanHeight(){
             return this.meanHeight;
         }
 
+        //Getter for the average roughness
         public double getAvgRoughness(){
             return this.avgRoughness;
         }
 
+        //Getter for the root mean square roughness
         public double getRootMeanSqRoughness(){
             return this.rootMeanSqRoughness;
         }
 
+        //Getter for Mu
         public double getMu(){
             return this.Mu;
         }
 
+        //Adds a Measurement to the data list for the test
         public void addDataPoint(int ID, double X, double Y, double height){
             this.data.Add(new Measurement(ID, X, Y, height));
             this.numMeasurements++;
         }
 
+        //Checks to see if the test has enough measurements to be considered a valid test
         public void checkValid(){
+            //If a test has exactly CONSIDERED_VALID measurements it is a valid test, any more or less and it is invalid
             if(this.numMeasurements == CONSIDERED_VALID){
                 this.isValidTest = true;
             }else{
@@ -93,6 +116,7 @@ namespace Factory_Systems{
             }
         }
 
+        //Gets the maximum and minimum height of the test from the database
         public void findMaxMinHeight(SQLiteConnection con){
             
             //Setting up two SQLiteCommands to run a query to get max height and get min height
@@ -139,7 +163,8 @@ namespace Factory_Systems{
 
         }
 
-        public double findMeanHeight(SQLiteConnection con){
+        //Gets the mean height from the database for the test
+        public void findMeanHeight(SQLiteConnection con){
 
             using var meanCmd = new SQLiteCommand("SELECT AVG(height) FROM Measurements WHERE test_uid = " + this.getID() + ";", con);
 
@@ -149,8 +174,6 @@ namespace Factory_Systems{
             //Set the maxHeight for this test
             this.meanHeight = meanReader.GetDouble(0);
 
-            return this.meanHeight;
-            
             /*************************************************************************************
 
             Another way I had thought about going to accomplish this before deciding it be better
@@ -169,52 +192,72 @@ namespace Factory_Systems{
             *************************************************************************************/
         }
 
-        public double findHeightRange(){
+
+        //Calculates the height range for the test
+        public void findHeightRange(){
             //Return the max height - min height, this will give the range of heights
             this.heightRange = this.maxHeight.getHeight() - this.minHeight.getHeight();
-            return this.heightRange;
         }
 
-        public void calcAvgRoughness(){
-            int m = 10;
-            int n = 10;
 
+        //Calculates the average roughness for the test
+        public void calcAvgRoughness(){
+            //Call to calcMu()
+            calcMu();
+
+            //A running total of the summation
             double runningTotal = 0;
+            //loop through the entire list of measurements for this test
             for(int i = 0; i < this.data.Count; i++){
-                runningTotal += Math.Abs(this.getData[i].getHeight() - this.getMu());
+                //Increase runningTotal by the necessary value
+                runningTotal += Math.Abs(this.getData()[i].getHeight() - this.getMu());
             }
 
-            this.avgRoughness = runningTotal / (m * n);
+            //Set avgRoughness by dividing by MN since that is the same as multiplying by 1/MN
+            this.avgRoughness = runningTotal / (M * N);
 
         }
 
+        //Calculates the root mean square roughness for the test
         public void calcRootMeanSqRoughness(){
-            int m = 10;
-            int n = 10;
+            //Call to calcMu()
+            calcMu();
+            //The power to raise Z(X,Y) - Mu to
             int power = 2;
 
+            //A running total of the summation
             double runningTotal = 0;
+            //Loop through the entire list of measurements for this test
             for(int i = 0; i < this.data.Count; i++){
-                runningTotal += Math.Pow(this.getData[i].getHeight() - this.getMu(), power);
+                //Increase runningTotal by the necessary value
+                runningTotal += Math.Pow(this.getData()[i].getHeight() - this.getMu(), power);
             }
 
-            runningTotal = runningTotal / (m * n);
+            //Divide by MN since that is the same as multiplying by 1/MN
+            runningTotal = runningTotal / (M * N);
 
+            //Raise the value to the power of 0.5
             double temp = Math.Pow(runningTotal, 0.5);
 
+            //Set rootMeanSqRoughness
             this.rootMeanSqRoughness = temp;
         }
 
+        //Calculates Mu to use in the root mean square roughness and average roughness calculations
         private void calcMu(){
             //Calculate 𝜇
-            int m = 10;
-            int n = 10;
+
+            //A running total of the summation
             double runningTotal = 0;
 
+            //loop through the entire list of measurements for this test
             for(int i = 0; i < this.data.Count; i++){
-                runningTotal += this.getData[i].getHeight();
+                //Increase runningTotal by the necessary value
+                runningTotal += this.getData()[i].getHeight();
             }
-            this.Mu = runningTotal / (m * n);
+
+            //Divide by MN since that is the same as multiplying by 1/MN
+            this.Mu = runningTotal / (M * N);
         }
     }
 }
